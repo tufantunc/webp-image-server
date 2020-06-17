0652f06045a20fa07833273db3ccf76488092ab4
Tufan Tunç <tufan.tunc@nesine.com>
2020-06-17 17:57:53 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # webp-image-server
 A very basic image server with webp browser support checker
+
+If request comes from a modern browser (with webp support)
+server sends webp formatted file,
+otherwise send jpg file.
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const { matchesUA } = require('browserslist-useragent');
+const useragent = require('express-useragent');
+const path = require('path');
+ 
+const app = express();
+app.use(useragent.express());
+
+function browserHasWebpSupport(userAgentSource) {
+  return matchesUA(userAgentSource, {
+    allowHigherVersions: true
+  });
+}
+
+app.get('/images/:file', (request,response) => {
+  let file;
+  if (browserHasWebpSupport(request.useragent.source)) {
+    file = `${request.params.file.replace(/.jpg|.png/, '.webp')}`;
+  } else {
+    file = request.params.file;
+  }
+
+  response.sendFile(file, {
+    root: path.join(__dirname, 'images')
+  });
+});
+
+app.listen(3000, () => {
+  console.log("App listening on port: 3000");
+});
